36bd6066b31f0fee9a9bfb22aa784713604d9e40
Aryan Raj <109361430+AryanRaj001@users.noreply.github.com>
2023-10-26 16:48:11 +0530

    Update DeleteBook.jsx

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -14,7 +14,8 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`http://localhost:5555/books/${id}`)
+      // .delete(`http://localhost:5555/books/${id}`)
+      .delete(`https://book-store-mern-api.vercel.app/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Deleted successfully', { variant: 'success' });
